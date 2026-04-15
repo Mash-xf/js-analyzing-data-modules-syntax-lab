cd75288895f7a983fbfd9e4b602676dd940c654e
Mash_xf <felix.macharia@student.moringaschool.com>
2026-04-15 22:09:28 +0300

    fist commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
 
+  for (const users of args) {
+    combinedObject.users = [...combinedObject.users, ...users];
+  }
+
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers,
 };
